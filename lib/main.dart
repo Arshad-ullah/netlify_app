0dc2053fd4f337a44c1849dee184dfb53b809b4f
Arshad <71887229+Arshad-ullah@users.noreply.github.com>
2022-10-06 17:44:27 +0500

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fluter data',
+      title: 'flutter add data',
       theme: ThemeData(
         // This is the theme of your application.
         //
